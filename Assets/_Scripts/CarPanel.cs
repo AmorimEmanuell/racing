a9c552769146e6f2d648e125a0a3a99633531342
Emanuell Amorim <amorimnuell@gmail.com>
2021-11-06 19:49:27 -0400

    Separate the velocity value in its own panel
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

--- Assets/_Scripts/CarPanel.cs
+++ Assets/_Scripts/CarPanel.cs
@@ -4,24 +4,16 @@ using UnityEngine;
 
 public class CarPanel : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI _velocity;
     [SerializeField] private TextMeshProUGUI _elapsedTime;
 
     private void OnEnable()
     {
-        CarData.Velocity.OnChange += UpdateVelocity;
         GameData.ElapsedTime.OnChange += UpdateElapsedTime;
     }
 
     private void OnDisable()
     {
-        CarData.Velocity.OnChange -= UpdateVelocity;
         GameData.ElapsedTime.OnChange -= UpdateElapsedTime;
-    }
-
-    private void UpdateVelocity(float velocity)
-    {
-        _velocity.text = velocity.ToString("F2");
     }
 
     private void UpdateElapsedTime(float elapsedTime)
---
+++ Assets/_Scripts/VelocityPanel.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class VelocityPanel : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _velocity;
+
+    private void OnEnable()
+    {
+        CarData.Velocity.OnChange += UpdateVelocity;
+    }
+
+    private void OnDisable()
+    {
+        CarData.Velocity.OnChange -= UpdateVelocity;
+    }
+
+    private void UpdateVelocity(float velocity)
+    {
+        _velocity.text = velocity.ToString("F2");
+    }
+}
