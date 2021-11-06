fcf7a8dfbb0e4898bb30374429e6a423b7700d02
Emanuell Amorim <amorimnuell@gmail.com>
2021-11-06 14:28:56 -0400

    Create first level with obstacles and offroad
    * Created the Road class to diferentiate from main road and off road.
    * When the player is on the offroad, we reduce it's max velocity.
    
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

--- Assets/_Scripts/CarController.cs
+++ Assets/_Scripts/CarController.cs
@@ -1,4 +1,5 @@
 ﻿#pragma warning disable 0649
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CarController : MonoBehaviour
@@ -10,11 +11,20 @@ public class CarController : MonoBehaviour
     [SerializeField] [Range(0.01f, 1f)] private float _turnVelocity = 0.1f;
 
     private float _velocity;
+    private RaycastHit[] _hitResults = new RaycastHit[1];
+    private Dictionary<Collider, Road> _cachedRoads = new Dictionary<Collider, Road>();
+    private int _roadLayerMask;
+
+    private void Awake()
+    {
+        _roadLayerMask = LayerMask.GetMask("Road");
+    }
 
     private void FixedUpdate()
     {
+        var maxVelocityMultiplier = GetMaxVelocityMultiplier();
         _velocity += _acceleration * Time.fixedDeltaTime;
-        _velocity = Mathf.Clamp(_velocity, 0, _maxVelocity);
+        _velocity = Mathf.Clamp(_velocity, 0, _maxVelocity * maxVelocityMultiplier);
         _carRigibody.velocity = _carTransform.forward * _velocity;
     }
 
@@ -27,5 +37,25 @@ public class CarController : MonoBehaviour
     public void ObstacleHit()
     {
         _velocity /= 2f;
+    }
+
+    private float GetMaxVelocityMultiplier()
+    {
+        var ray = new Ray(_carTransform.position, -_carTransform.up);
+        var hits = Physics.RaycastNonAlloc(ray, _hitResults, 1f, _roadLayerMask);
+        if (hits == 0)
+        {
+            return 1f;
+        }
+
+        var hit = _hitResults[0];
+        if (_cachedRoads.TryGetValue(hit.collider, out Road road))
+        {
+            return road.MaxVelocityMultiplier;
+        }
+
+        var newlyDiscoveredRoad = hit.collider.GetComponent<Road>();
+        _cachedRoads.Add(hit.collider, newlyDiscoveredRoad);
+        return newlyDiscoveredRoad.MaxVelocityMultiplier;
     }
 }
---
+++ Assets/_Scripts/Road.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+    [SerializeField] [Range(0.1f, 10f)] private float _maxVelocityMultiplier = 1f;
+
+    public float MaxVelocityMultiplier => _maxVelocityMultiplier;
+}
