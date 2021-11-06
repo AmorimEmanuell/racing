734bc67c911de3f3d667cafa5011bdc40210f7b2
Emanuell Amorim <amorimnuell@gmail.com>
2021-11-06 17:48:19 -0400

    Create the EventBus system
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

---
+++ Assets/_Scripts/EventBus.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class EventBus
+{
+    public enum EventType
+    {
+        CheckpointReached
+    }
+
+    private static Dictionary<EventType, List<Action>> _registeredEvents = new Dictionary<EventType, List<Action>>();
+
+    public static void Register(EventType eventType, Action action)
+    {
+        if (_registeredEvents.TryGetValue(eventType, out List<Action> registeredActions))
+        {
+            registeredActions.Add(action);
+            return;
+        }
+
+        _registeredEvents.Add(eventType, new List<Action> { action });
+    }
+
+    public static void Unregister(EventType eventType, Action action)
+    {
+        if (_registeredEvents.TryGetValue(eventType, out List<Action> registeredActions))
+        {
+            registeredActions.Remove(action);
+        }
+    }
+
+    public static void Trigger(EventType eventType)
+    {
+        if (_registeredEvents.TryGetValue(eventType, out List<Action> registeredActions))
+        {
+            for (var i=0; i<registeredActions.Count; i++)
+            {
+                registeredActions[i].Invoke();
+            }
+        }
+    }
+}
