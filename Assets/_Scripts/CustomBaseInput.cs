7deb1c1ec5149d665fa9d568b2a9807d6b7a0ae7
Emanuell Amorim <amorimnuell@gmail.com>
2021-11-07 09:05:43 -0400

    Create the CustomBaseInput and CustomInputModule
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

---
+++ Assets/_Scripts/CustomBaseInput.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CustomBaseInput : BaseInput
+{
+    public override Vector2 mousePosition => new Vector2(Screen.width/2, Screen.height/2);
+}
---
+++ Assets/_Scripts/CustomInputModule.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(CustomBaseInput))]
+public class CustomInputModule : StandaloneInputModule
+{
+    protected override void Start()
+    {
+        inputOverride = GetComponent<CustomBaseInput>();
+        base.Start();
+    }
+}
