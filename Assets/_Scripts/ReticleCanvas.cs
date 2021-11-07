3432bf828694c093bd64e312fb6a6778b3939b3c
Emanuell Amorim <amorimnuell@gmail.com>
2021-11-07 09:11:49 -0400

    Add a reticle to improve UI interaction experience
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

---
+++ Assets/_Scripts/ReticleCanvas.cs
@@ -0,0 +1,29 @@
+﻿#pragma warning disable 0649
+using UnityEngine;
+
+public class ReticleCanvas : MonoBehaviour
+{
+    [SerializeField] private GameObject _reticle;
+
+    private void OnEnable()
+    {
+        EventBus.Register(EventBus.EventType.GamePaused, OnGamePaused);
+        EventBus.Register(EventBus.EventType.GameUnpaused, OnGameUnpaused);
+    }
+
+    private void OnDisable()
+    {
+        EventBus.Unregister(EventBus.EventType.GamePaused, OnGamePaused);
+        EventBus.Unregister(EventBus.EventType.GameUnpaused, OnGameUnpaused);
+    }
+
+    private void OnGamePaused()
+    {
+        _reticle.SetActive(true);
+    }
+
+    private void OnGameUnpaused()
+    {
+        _reticle.SetActive(false);
+    }
+}
