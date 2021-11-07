7ec95977f4810875a3c497cca2943489bc20d82c
Emanuell Amorim <amorimnuell@gmail.com>
2021-11-07 10:25:44 -0400

    Fix issue with reticle not disappearing when restarting game
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

--- Assets/_Scripts/UI/ReticleCanvas.cs
+++ Assets/_Scripts/UI/ReticleCanvas.cs
@@ -9,12 +9,14 @@ public class ReticleCanvas : MonoBehaviour
     {
         EventBus.Register(EventBus.EventType.PauseGame, OnGamePaused);
         EventBus.Register(EventBus.EventType.UnpauseGame, OnGameUnpaused);
+        EventBus.Register(EventBus.EventType.RestartGame, OnGameRestarted);
     }
 
     private void OnDisable()
     {
         EventBus.Unregister(EventBus.EventType.PauseGame, OnGamePaused);
         EventBus.Unregister(EventBus.EventType.UnpauseGame, OnGameUnpaused);
+        EventBus.Register(EventBus.EventType.RestartGame, OnGameRestarted);
     }
 
     private void OnGamePaused(object args)
@@ -23,6 +25,11 @@ public class ReticleCanvas : MonoBehaviour
     }
 
     private void OnGameUnpaused(object obj)
+    {
+        _reticle.SetActive(false);
+    }
+
+    private void OnGameRestarted(object obj)
     {
         _reticle.SetActive(false);
     }
