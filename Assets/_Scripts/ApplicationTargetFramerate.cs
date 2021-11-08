6de6d6c137b7d5fbd27f79d912af45cd98e50abe
Emanuell Amorim <amorimnuell@gmail.com>
2021-11-07 22:24:18 -0400

    Set application target framerate to 90
    Signed-off-by: Emanuell Amorim <amorimnuell@gmail.com>

---
+++ Assets/_Scripts/ApplicationTargetFramerate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ApplicationTargetFramerate : MonoBehaviour
+{
+    [SerializeField] private int _target = 90;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = _target;
+    }
+}
